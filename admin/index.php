a864b03193c507850a97732e139498e15d91dcf4
Doglas <doglaska@gmail.com>
2025-12-03 05:01:57 -0300

    Estilizando as paginas

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,38 @@
-<?php
+<link rel="stylesheet" href="../assets/css/admin.css">
+<link rel="stylesheet" href="../assets/css/style.css">
 
-    echo "<h1>Painel Administrativo</h1>";
+<div class="admin-header">
+    <nav class="navbar navbar-expand-lg shadow-sm py-3">
+        <div class="container-logo">
+            <a class="navbar-brand d-flex align-items-center" href="?pg=index">
+                <img src="../assets/img/logo.png" class="logo-image" alt="Logo">
+                <span class="admin-span">Mister Bank</span>
+            </a>
+        </div>
+    </nav>
+</div>
 
-    echo "<a href='?pg=controller/adminHome'>Listar Clientes</a> | ";
-    echo "<a href='?pg=controller/form'>Cadastrar Clientes</a> | ";
-    echo "<hr>";
+<div class="admin-menu">
+    <a href='?pg=controller/adminHome'>Listar Clientes</a> 
+    <a href='?pg=controller/form'>Cadastrar Clientes</a>
+</div>
 
-    if(empty($_SERVER['QUERY_STRING'])){
-       echo "<h3>Bem-vindo ao painel admin.";
-    }else {
-        $pg = "$_GET[pg]";
-        include_once "$pg.php";
-    }
+<div class="admin-content">
+    <?php
+        if(empty($_SERVER['QUERY_STRING'])){
+        echo "<h3>Bem-vindo ao painel admin.";
+        }else {
+            $pg = "$_GET[pg]";
+            include_once "$pg.php";
+        }
+    ?>
+</div>
+
+<footer class="footer-container">
+    <div class="container d-flex align-items-center py-3">
+        <a class="navbar-brand d-flex align-items-center" href="?pg=index">
+            <img src="../assets/img/logo.png" class="logo-image" alt="Logo do Sistema">
+            <span class="admin-span">Mister Bank</span>
+        </a>
+    </div>
+</footer>
